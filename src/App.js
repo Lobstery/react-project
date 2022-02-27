9f4fa75e0eb74fa0ed8c8e6a07b65c076df90da2
Lobstery <56125573+Lobstery@users.noreply.github.com>
2022-02-27 17:41:53 +0200

    MyButton

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 import './styles/App.css';
 
 function App() {
-    const [value, setValue] = useState('Johnny');
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'Javascript', body: 'Description' },
+        { id: 2, title: 'Javascript1', body: 'Description' },
+        { id: 3, title: 'Javascript2', body: 'Description' },
+    ]);
 
     return (
         <div className="App">
-            <PostItem />
-            <PostItem />
-            <PostItem />
-            <PostItem />
-            <Counter />
+            <form>
+                <input type="text" placeholder="Denumire Post" />
+                <input type="text" placeholder="Descriere Post" />
+                <MyButton disabled>Creare post</MyButton>
+            </form>
+            <PostList posts={posts} title="Lista 1" />
         </div>
     );
 }
