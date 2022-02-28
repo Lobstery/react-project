3c33684fcc541f88f6665012a2f51292d32c2550
Lobstery <56125573+Lobstery@users.noreply.github.com>
2022-02-28 15:12:19 +0200

    SearchMemo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React, { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
-import PostItem from "./components/PostItem";
+import React, { useMemo, useState } from "react";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
+
 import './styles/App.css';
 
 function App() {
     const [posts, setPosts] = useState([
-        { id: 1, title: 'Javascript', body: 'Description' },
-        { id: 2, title: 'Javascript1', body: 'Description' },
-        { id: 3, title: 'Javascript2', body: 'Description' },
+        { id: 1, title: 'Aaaaaa', body: 'Bbbbbbbbb' },
+        { id: 2, title: 'Cccccc', body: 'Aaaaaaaaa' },
+        { id: 3, title: 'Bbbbbb', body: 'Ccccccccc' },
     ]);
+    const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
 
+    const sortedPosts = useMemo(() => {
+        console.log('Functia de observare a posturilor sortate');
+        if (selectedSort) {
+            return [...posts,].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        }
+        else return posts;
+    }, [selectedSort, posts]);
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()));
+    }, [searchQuery, sortedPosts]);
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost]);
+    }
+    //---Functie CallBack
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+    }
     return (
         <div className="App">
-            <form>
-                <input type="text" placeholder="Denumire Post" />
-                <input type="text" placeholder="Descriere Post" />
-                <MyButton disabled>Creare post</MyButton>
-            </form>
-            <PostList posts={posts} title="Lista 1" />
+            <PostForm create={createPost} />
+            <hr style={{ margin: "15px 0" }} />
+            <div>
+                <MyInput
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    placeholder="Search"
+                />
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Sortarea"
+                    options={[
+                        { value: "title", name: "Dupa titlu" },
+                        { value: "body", name: "Dupa descriere" },
+                    ]} />
+            </div>
+            {
+                sortedAndSearchedPosts.length
+                    ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Lista Cu Posturi" />
+                    : <h1 style={{ textAlign: "center" }}>Nu s-a gasit nici-un post</h1>
+            }
+
         </div>
     );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import PostItem from "./PostItem";
-const PostList = ({ posts, title }) => {
+const PostList = ({ remove, posts, title }) => {
     return (
         <div>
             <h1 style={{ textAlign: 'center' }}>{title}</h1>
-            {posts.map(post =>
-                <PostItem post={post} key={post.id} />
+            {posts.map((post, index) =>
+                <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
             )}
         </div>
     );
