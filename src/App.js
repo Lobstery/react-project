79bb50a18000847c59cf74f8209c7430a8ef765b
Lobstery <56125573+Lobstery@users.noreply.github.com>
2022-02-28 16:19:42 +0200

    Posts Animated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useMemo, useState } from "react";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyInput from "./components/UI/input/MyInput";
-import MySelect from "./components/UI/select/MySelect";
+import MyButton from "./components/UI/button/MyButton";
+import MyModal from "./components/UI/modal/MyModal";
 
 import './styles/App.css';
 
@@ -12,56 +13,40 @@ function App() {
         { id: 2, title: 'Cccccc', body: 'Aaaaaaaaa' },
         { id: 3, title: 'Bbbbbb', body: 'Ccccccccc' },
     ]);
-    const [selectedSort, setSelectedSort] = useState('');
-    const [searchQuery, setSearchQuery] = useState('');
+    const [filter, setFilter] = useState({ sort: '', query: '' });
+    const [modal, setModal] = useState(false);
 
     const sortedPosts = useMemo(() => {
         console.log('Functia de observare a posturilor sortate');
-        if (selectedSort) {
-            return [...posts,].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        if (filter.sort) {
+            return [...posts,].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
         }
         else return posts;
-    }, [selectedSort, posts]);
+    }, [filter.sort, posts]);
 
     const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()));
-    }, [searchQuery, sortedPosts]);
+        return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(filter.query.toLocaleLowerCase()));
+    }, [filter.query, sortedPosts]);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
+        setModal(false);
     }
     //---Functie CallBack
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id));
     }
-    const sortPosts = (sort) => {
-        setSelectedSort(sort);
-    }
     return (
         <div className="App">
-            <PostForm create={createPost} />
+            <MyButton style={{ margin: "30px 0 0 0" }} onClick={() => setModal(true)}>
+                Creare Post
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost} />
+            </MyModal>
             <hr style={{ margin: "15px 0" }} />
-            <div>
-                <MyInput
-                    value={searchQuery}
-                    onChange={e => setSearchQuery(e.target.value)}
-                    placeholder="Search"
-                />
-                <MySelect
-                    value={selectedSort}
-                    onChange={sortPosts}
-                    defaultValue="Sortarea"
-                    options={[
-                        { value: "title", name: "Dupa titlu" },
-                        { value: "body", name: "Dupa descriere" },
-                    ]} />
-            </div>
-            {
-                sortedAndSearchedPosts.length
-                    ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Lista Cu Posturi" />
-                    : <h1 style={{ textAlign: "center" }}>Nu s-a gasit nici-un post</h1>
-            }
-
+            <PostFilter filter={filter} setFilter={setFilter} />
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Lista Cu Posturi" />
         </div>
     );
 }
