a5a1bcad13b607d229437d53cc30065ddaffb6ec
Lobstery <56125573+Lobstery@users.noreply.github.com>
2022-02-28 19:35:17 +0200

    Loader

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
+import PostService from "./API/PostService";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import Loader from "./components/UI/loader/Loader";
 import MyModal from "./components/UI/modal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 
 import './styles/App.css';
 
 function App() {
-    const [posts, setPosts] = useState([
-        { id: 1, title: 'Aaaaaa', body: 'Bbbbbbbbb' },
-        { id: 2, title: 'Cccccc', body: 'Aaaaaaaaa' },
-        { id: 3, title: 'Bbbbbb', body: 'Ccccccccc' },
-    ]);
+    const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({ sort: '', query: '' });
     const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [isPostLoading, setIsPostLoading] = useState(false);
 
-    const sortedPosts = useMemo(() => {
-        console.log('Functia de observare a posturilor sortate');
-        if (filter.sort) {
-            return [...posts,].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        }
-        else return posts;
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(filter.query.toLocaleLowerCase()));
-    }, [filter.query, sortedPosts]);
+    useEffect(() => { fetchPosts(); }, []);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setModal(false);
     }
-    //---Functie CallBack
+
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id));
     }
+
+    async function fetchPosts() {
+        setIsPostLoading(true);
+        setTimeout(async () => {
+            const posts = await PostService.getAll();
+            setPosts(posts);
+            setIsPostLoading(false);
+        }, 3000);
+    }
+
+
     return (
         <div className="App">
             <MyButton style={{ margin: "30px 0 0 0" }} onClick={() => setModal(true)}>
@@ -46,7 +48,14 @@ function App() {
             </MyModal>
             <hr style={{ margin: "15px 0" }} />
             <PostFilter filter={filter} setFilter={setFilter} />
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Lista Cu Posturi" />
+            {
+                isPostLoading
+                    ?
+                    <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
+                        <Loader></Loader>
+                    </div>
+                    : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Lista Cu Posturi" />
+            }
         </div>
     );
 }
